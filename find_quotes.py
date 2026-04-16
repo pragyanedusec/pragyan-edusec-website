883ccee469c911dbc272f00c7b1821d734f5a970
marvin222-debug <marvinraichur14@gmail.com>
2026-04-17 02:08:18 +0530

    Chore: clean up diagnostic tools

--- find_quotes.py
+++
@@ -1,8 +0,0 @@
-with open(r'd:\pragyan-edusec-website\about.html', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        if 'ecosystem ""' in line or 'innovation ""' in line or 'technology ""' in line:
-            print(f'L{i+1}: {line.strip()[:100]}')
-        if 'â–¹' in line:
-            print(f'L{i+1}: contains bullet-like char')
-        if 'âˆž' in line:
-            print(f'L{i+1}: contains infinity-like char')
